c272777aabd1ed0b17c9f6431be75a47f12254e3
Esger <esgerj@gmail.com>
2024-09-21 02:28:58 +0200

    Dubbele titel weg

--- src/resources/elements/game-state.js
+++ src/resources/elements/game-state.js
@@ -1,9 +1,8 @@
-import {bindable} from 'aurelia-framework';
+import { bindable } from 'aurelia-framework';
 
 export class GameState {
   @bindable value;
-  title = 'Wie doet er mee?'
-  gameStates=['names', 'win', 'lose'];
+  gameStates = ['names', 'win', 'lose'];
 
   constructor() {
     this._gameState = this.gameStates[0];
