85725c4b7cabc096e2e8ff5fc7683f5dd589d68e
Esger <esgerj@gmail.com>
2024-09-21 04:17:20 +0200

    Game met random letter

--- src/resources/elements/game.js
+++ src/resources/elements/game.js
@@ -3,7 +3,15 @@ import { bindable } from 'aurelia-framework';
 export class Game {
   @bindable name;
 
-  valueChanged(newValue, oldValue) {
+  attached() {
+    this.randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  }
+
+  gameResult() {
+    //
+  }
+
+  bounce() {
     //
   }
 }
