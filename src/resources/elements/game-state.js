4c8db8d7e1a32d5352bdbebdc9d2181d845fcb87
Esger <esgerj@gmail.com>
2024-09-21 03:32:18 +0200

    Begin knop next game state

--- src/resources/elements/game-state.js
+++ src/resources/elements/game-state.js
@@ -1,12 +1,24 @@
-import { bindable } from 'aurelia-framework';
-
+import { bindable, inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+@inject(EventAggregator)
 export class GameState {
   @bindable value;
   gameStates = ['names', 'win', 'lose'];
 
-  constructor() {
+  constructor(EventAggregator) {
+    this._eventAggregator = EventAggregator;
     this.state = 0;
     this._gameState = this.gameStates[this._state];
+  }
+
+  attached() {
+    this._startSubscription = this._eventAggregator.subscribe('start', names => {
+      this.state = 1;
+    })
+  }
+
+  detached() {
+    this._startSubscription.dispose();
   }
 
   enterNames(gameState) {
--- src/resources/elements/names.js
+++ src/resources/elements/names.js
@@ -1,14 +1,23 @@
-import { bindable } from 'aurelia-framework';
-
+import { bindable, inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+@inject(EventAggregator)
 export class Names {
   @bindable names = [];
   title = 'Wie doet er mee?';
   minLength = 3;
+
+  constructor(EventAggregator) {
+    this._eventAggregator = EventAggregator;
+  }
 
   addName(name) {
     const isLongEnough = name.length >= this.minLength;
     if (!isLongEnough) return;
     this.names.push(name);
     this.name = '';
+  }
+
+  start() {
+    this._eventAggregator.publish('start', this.names);
   }
 }
