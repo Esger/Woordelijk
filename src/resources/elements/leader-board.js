7d7fe999bd4bed7acea75a897305b7dc07172c08
Esger <esgerj@gmail.com>
2024-10-04 21:07:20 +0200

    disable settings during letterReady

--- src/resources/elements/leader-board.js
+++ src/resources/elements/leader-board.js
@@ -3,7 +3,7 @@ import { bindable } from 'aurelia-framework';
 export class LeaderBoardCustomElement {
     @bindable persons;
     @bindable state = 0;
-
+    @bindable letterReady = false;
 
     showLeaderBoard() {
         $('leader-board dialog')[0].showModal();
