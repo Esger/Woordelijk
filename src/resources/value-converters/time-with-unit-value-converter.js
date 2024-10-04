59e1fe0a75377ab68f494c1e916d59bbd9e8a2b2
Esger <esgerj@gmail.com>
2024-10-04 20:50:07 +0200

    pad seconds after minutes

--- src/resources/value-converters/time-with-unit-value-converter.js
+++ src/resources/value-converters/time-with-unit-value-converter.js
@@ -2,7 +2,8 @@ export class TimeWithUnitValueConverter {
     toView(time, short = false) {
         if (!time) return;
         if (time < 60) return Math.floor(time) + ' s';
-        else return Math.floor(time / 60) + ':' + Math.floor(time % 60) + ' m';
+        else return Math.floor(time / 60) + ':' + (Math.floor(time % 60)).toString().padStart(2, '0')
+            + ' m';
     }
 
     fromView(time) {
