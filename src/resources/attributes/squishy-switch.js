8b9cd8a48d7f6f7a964cf66f45bbbc30113faf8d
Esger <esgerj@gmail.com>
2024-10-04 20:54:58 +0200

    prevent double init squishy-switch

--- src/resources/attributes/squishy-switch.js
+++ src/resources/attributes/squishy-switch.js
@@ -7,6 +7,8 @@ export class SquishySwitchCustomAttribute {
     }
 
     bind() {
+        const hasCandy = $(this._element).closest('.toggle-container').length;
+        if (hasCandy) return;
         const toggleTrack = '<div class="toggle-track"><div class="toggle-thumb"></div></div>'
         const wrapperHtml = '<div class="toggle-container green"></div>';
         $(this._element).wrap(wrapperHtml).after(toggleTrack).addClass('toggle-checkbox');
