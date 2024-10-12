10cc531b9c25c164fd002fa4b5f5c3fc43e764e2
Esger <esgerj@gmail.com>
2024-10-12 17:01:54 +0200

    scale only persons start screen

--- src/app.js
+++ src/app.js
@@ -6,4 +6,18 @@ export class App {
     constructor(keyInputService) {
         this._keyInputService = keyInputService;
     }
+
+    attached() {
+        $(window.visualViewport).on('resize', _ => requestAnimationFrame(this._scaleGame));
+        this._scaleGame();
+    }
+
+    _scaleGame() {
+        const width = window.visualViewport.width;
+        const scale = (width - 32) / (width);
+        if (document.documentElement.clientWidth < 600) {
+            document.body.style.setProperty('--scale', scale)
+        }
+    }
+
 }
